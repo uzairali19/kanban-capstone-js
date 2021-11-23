f8e44a2abd345f51c80ff045af4e961f32c827a9
Uzair Ali <uzairali19b@gmail.com>
2021-11-23 20:23:39 +0500

    update api data front-end

--- src/index.js
+++ src/index.js
@@ -12,4 +12,53 @@ const getData = async () => {
   return scores;
 };
 
-getData();
+const mainBody = document.querySelector('#body');
+
+getData().then((v) => {
+  for (let i = 0; i < 15; i++) {
+    const bodyItems = `<article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img
+            src="${v[i].url}"
+          />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <p>${v[i].name}</p>
+        </div>
+        <nav class="level is-mobile">
+          <div class="level-left">
+            <a class="level-item">
+              <span class="icon is-small"
+                ><i class="fa fa-heart"></i>
+              </span>
+              <p class="like-text">like</p>
+            </a>
+          </div>
+        </nav>
+        <div class="level">
+          <button class="button is-primary is-small">Comments</button>
+          <button class="button is-warning is-small">
+            Reservations
+          </button>
+        </div>
+      </div>
+      </article>`;
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('main-div');
+    mainDiv.innerHTML = bodyItems;
+    mainBody.appendChild(mainDiv);
+  }
+});
+
+// btn.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   modal.classList.toggle('show');
+// });
+
+// closeModal.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   modal.classList.remove('show');
+// });
