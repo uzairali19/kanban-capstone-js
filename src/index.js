a317607815adac6c7e49946815bb0580564a2c78
gracekabaghe <gracema2002@gmail.com>
2021-11-25 13:05:22 +0200

    Reservations popup setup

--- src/index.js
+++ src/index.js
@@ -39,8 +39,14 @@ getData().then((v) => {
           </div>
         </nav>
         <div class="level">
-          <button class="button is-primary is-small">Comments</button>
-          <button class="button is-warning is-small">
+          <button
+            class="modal-button button is-primary is-small"
+            data-target="modal"
+            aria-haspopup="true"
+            >
+            Comments
+            </button>
+          <button id="${v[i].id}" class="reservations button is-warning is-small">
             Reservations
           </button>
         </div>
@@ -51,14 +57,49 @@ getData().then((v) => {
     mainDiv.innerHTML = bodyItems;
     mainBody.appendChild(mainDiv);
   }
+  const resModal = document.querySelector('#res-modal');
+
+  const reserve = document.querySelectorAll('.reservations');
+  reserve.forEach((res) => {
+    res.addEventListener('click', (e) => {
+      e.preventDefault();
+      const cardItem = e.target;
+      for (let i = 0; i < 15; i++) {
+        if (v[i].id === cardItem.id) {
+          const resItem = `
+            <div class="modal-background"></div>
+            <div class="modal-content">
+              <img class="res-img" src="${v[i].url}" alt="">
+              <p class="res-meme-name">${v[i].name}</p>
+              <div class="res-list">
+                <p class="start-date">03/11/2021</p>
+                <p>-</p>
+                <p class="start-end">03/12/2021</p>
+                <p class="res-name">Henry G</p>
+              </div>
+              <h3 class="form-content subtitle">
+                Add a reservation
+              </h3>
+              <form class="res-form" action="">
+                <input class="input is-primary" type="text" placeholder="Your Name"> <br>
+                <input class="input is-primary" type="date" name="" id="start-date"> <br>
+                <input class="input is-primary" type="date" name="" id="end-date"> <br>
+              </form>
+              <button class="res-button button is-primary" id="reservation-button">Reservation</button>
+            </div>
+            <button id="close-modal" class="modal-close is-large" aria-label="close"></button>`;
+          resModal.innerHTML = resItem;
+          resModal.classList.toggle('is-active');
+        }
+      }
+      if (resModal.classList.contains('is-active')) {
+        const closeCard = document.querySelector('#close-modal');
+        closeCard.addEventListener('click', (e) => {
+          e.preventDefault();
+          resModal.classList.remove('is-active');
+        });
+      }
+      // get data from api
+    });
+  });
 });
-
-// btn.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   modal.classList.toggle('show');
-// });
-
-// closeModal.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   modal.classList.remove('show');
-// });
