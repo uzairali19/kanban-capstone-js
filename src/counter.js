fd9db6978cedfbbf075a3e65b1f18d4273a30755
Uzair Ali <uzairali19b@gmail.com>
2021-11-26 20:42:57 +0500

    update data in counter

--- src/counter.js
+++ src/counter.js
@@ -1,10 +1,10 @@
+const counter = (v) => v.childElementCount;
+
 const memeCount = (memeData, body) => {
   memeData.then((v) => {
     const memes = document.querySelector('#meme-counter');
-    const bodyCount = body.childElementCount;
-    memes.innerHTML = `Elements shown ${bodyCount}, Elements recieved ${v.length}`;
+    memes.innerHTML = `Elements shown ${counter(body)}, Elements recieved ${v.length}`;
   });
 };
 
-const counter = (v) => v.childElementCount;
 module.exports = { memeCount, counter };
