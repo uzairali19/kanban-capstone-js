ebea54ef63f78d44711e4ba81e54cbeea57602e5
Uzair Ali <uzairali19b@gmail.com>
2021-11-27 13:52:07 +0500

    update JSDOM

--- src/test/counter.test.js
+++ src/test/counter.test.js
@@ -1,7 +1,7 @@
 import { JSDOM } from 'jsdom';
 import { counter } from '../counter';
 
-const dom = new JSDOM(`<!DOCTYPE html><body><div id="itemList"></body>`); // eslint-disable-line
+const dom = new JSDOM(`<!DOCTYPE html><body><div id="itemList"></div></body>`); // eslint-disable-line
 
 global.document = dom.window.document;
 global.window = dom.window;
@@ -9,7 +9,7 @@ global.window = dom.window;
 const list = document.getElementById('itemList');
 
 test('test count of elements', () => {
-  const element = document.createElement('li');
+  const element = document.createElement('div');
   list.appendChild(element);
   expect(counter(list)).toBe(1);
 });
