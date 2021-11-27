1203fa9e0ae31224e7664aa5deee33e11ff36efa
gracekabaghe <gracema2002@gmail.com>
2021-11-27 10:38:52 +0200

    Removed comments

--- src/__mocks__/counter.js
+++ src/__mocks__/counter.js
@@ -1,6 +1,5 @@
 function numberOfReservations(content) {
   let myCount = content.length;
-  // const resNo = document.getElementById('resNo');
   if (myCount === undefined) {
     myCount = 0;
   }
