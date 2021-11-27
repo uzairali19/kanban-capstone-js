49e4dbaa5edc5e288a1694e1a6f685f4152dcbcf
gracekabaghe <gracema2002@gmail.com>
2021-11-27 09:28:13 +0200

    fix eslint error

--- src/__mocks__/counter.js
+++ src/__mocks__/counter.js
@@ -1,10 +1,10 @@
 function numberOfReservations(content) {
   let myCount = content.length;
-  const resNo = document.getElementById('resNo');
+  // const resNo = document.getElementById('resNo');
   if (myCount === undefined) {
     myCount = 0;
   }
   return myCount;
 }
 
-  export default numberOfReservations;
+export default numberOfReservations;
